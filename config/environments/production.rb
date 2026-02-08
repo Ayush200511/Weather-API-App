a543d9d25a26cd0520d05053ff766e16a8808744
Ayush Kumar <ayush.a09020@gmail.com>
2026-02-08 13:38:23 +0000

    Updated README with project details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+config.hosts << /render\.com/
+config.hosts << ENV.fetch("RENDER_EXTERNAL_HOSTNAME", nil)
 end
